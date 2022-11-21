a0a66a59930f40070ff30648a81494bdb5b68241
Alex Dima <alexdima@microsoft.com>
2022-11-21 14:57:15 +0000

    Switch back to mocha

--- out/test/index.test.js
+++ out/test/index.test.js
@@ -21,119 +21,115 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
+const assert = __importStar(require("assert"));
 const fs = __importStar(require("fs"));
 const path = __importStar(require("path"));
 const index_1 = require("../index");
-const tape_1 = __importDefault(require("tape"));
 const REPO_ROOT = path.join(__dirname, '../../');
 const wasm = fs.readFileSync(path.join(REPO_ROOT, './out/onig.wasm')).buffer;
 const loadPromise = index_1.loadWASM({ instantiator: (imports) => WebAssembly.instantiate(wasm, imports) });
 function testLib(name, callback) {
-    tape_1.default(name, async (t) => {
+    test(name, async () => {
         await loadPromise;
-        callback(t);
-        t.end();
+        callback();
     });
 }
-testLib('simple1', (t) => {
+testLib('simple1', () => {
     const scanner = new index_1.OnigScanner(['ell', 'wo']);
     const s = new index_1.OnigString('Hello world!');
-    t.deepEqual(scanner.findNextMatchSync(s, 0), { index: 0, captureIndices: [{ start: 1, end: 4, length: 3 }] });
-    t.deepEqual(scanner.findNextMatchSync(s, 2), { index: 1, captureIndices: [{ start: 6, end: 8, length: 2 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync(s, 0), { index: 0, captureIndices: [{ start: 1, end: 4, length: 3 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync(s, 2), { index: 1, captureIndices: [{ start: 6, end: 8, length: 2 }] });
     s.dispose();
     scanner.dispose();
 });
-testLib('simple2', (t) => {
+testLib('simple2', () => {
     const scanner = new index_1.OnigScanner(['a', 'b', 'c']);
-    t.deepEqual(scanner.findNextMatchSync('x', 0), null);
-    t.deepEqual(scanner.findNextMatchSync('xxaxxbxxc', 0), { index: 0, captureIndices: [{ start: 2, end: 3, length: 1 }] });
-    t.deepEqual(scanner.findNextMatchSync('xxaxxbxxc', 4), { index: 1, captureIndices: [{ start: 5, end: 6, length: 1 }] });
-    t.deepEqual(scanner.findNextMatchSync('xxaxxbxxc', 7), { index: 2, captureIndices: [{ start: 8, end: 9, length: 1 }] });
-    t.deepEqual(scanner.findNextMatchSync('xxaxxbxxc', 9), null);
+    assert.deepStrictEqual(scanner.findNextMatchSync('x', 0), null);
+    assert.deepStrictEqual(scanner.findNextMatchSync('xxaxxbxxc', 0), { index: 0, captureIndices: [{ start: 2, end: 3, length: 1 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync('xxaxxbxxc', 4), { index: 1, captureIndices: [{ start: 5, end: 6, length: 1 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync('xxaxxbxxc', 7), { index: 2, captureIndices: [{ start: 8, end: 9, length: 1 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync('xxaxxbxxc', 9), null);
     scanner.dispose();
 });
-testLib('unicode1', (t) => {
+testLib('unicode1', () => {
     const scanner1 = new index_1.OnigScanner(['1', '2']);
-    t.deepEqual(scanner1.findNextMatchSync('ab…cde21', 5), { index: 1, captureIndices: [{ start: 6, end: 7, length: 1 }] });
+    assert.deepStrictEqual(scanner1.findNextMatchSync('ab…cde21', 5), { index: 1, captureIndices: [{ start: 6, end: 7, length: 1 }] });
     scanner1.dispose();
     const scanner2 = new index_1.OnigScanner(['\"']);
-    t.deepEqual(scanner2.findNextMatchSync('{"…": 1}', 1), { index: 0, captureIndices: [{ start: 1, end: 2, length: 1 }] });
+    assert.deepStrictEqual(scanner2.findNextMatchSync('{"…": 1}', 1), { index: 0, captureIndices: [{ start: 1, end: 2, length: 1 }] });
     scanner2.dispose();
 });
-testLib('unicode2', (t) => {
+testLib('unicode2', () => {
     const scanner = new index_1.OnigScanner(['Y', 'X']);
-    t.deepEqual(scanner.findNextMatchSync('a💻bYX', 0), { index: 0, captureIndices: [{ start: 4, end: 5, length: 1 }] });
-    t.deepEqual(scanner.findNextMatchSync('a💻bYX', 1), { index: 0, captureIndices: [{ start: 4, end: 5, length: 1 }] });
-    t.deepEqual(scanner.findNextMatchSync('a💻bYX', 3), { index: 0, captureIndices: [{ start: 4, end: 5, length: 1 }] });
-    t.deepEqual(scanner.findNextMatchSync('a💻bYX', 4), { index: 0, captureIndices: [{ start: 4, end: 5, length: 1 }] });
-    t.deepEqual(scanner.findNextMatchSync('a💻bYX', 5), { index: 1, captureIndices: [{ start: 5, end: 6, length: 1 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync('a💻bYX', 0), { index: 0, captureIndices: [{ start: 4, end: 5, length: 1 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync('a💻bYX', 1), { index: 0, captureIndices: [{ start: 4, end: 5, length: 1 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync('a💻bYX', 3), { index: 0, captureIndices: [{ start: 4, end: 5, length: 1 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync('a💻bYX', 4), { index: 0, captureIndices: [{ start: 4, end: 5, length: 1 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync('a💻bYX', 5), { index: 1, captureIndices: [{ start: 5, end: 6, length: 1 }] });
     scanner.dispose();
 });
-testLib('unicode3', (t) => {
+testLib('unicode3', () => {
     const scanner = new index_1.OnigScanner(['Возврат']);
-    t.deepEqual(scanner.findNextMatchSync('Возврат long_var_name;', 0), { index: 0, captureIndices: [{ start: 0, end: 7, length: 7 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync('Возврат long_var_name;', 0), { index: 0, captureIndices: [{ start: 0, end: 7, length: 7 }] });
     scanner.dispose();
 });
-testLib('unicode4', (t) => {
+testLib('unicode4', () => {
     const scanner = new index_1.OnigScanner(['X']);
-    t.deepEqual(scanner.findNextMatchSync(`X${String.fromCharCode(0xd83c)}X`, 0), { index: 0, captureIndices: [{ start: 0, end: 1, length: 1 }] });
-    t.deepEqual(scanner.findNextMatchSync(`X${String.fromCharCode(0xd83c)}X`, 1), { index: 0, captureIndices: [{ start: 2, end: 3, length: 1 }] });
-    t.deepEqual(scanner.findNextMatchSync(`X${String.fromCharCode(0xd83c)}X`, 2), { index: 0, captureIndices: [{ start: 2, end: 3, length: 1 }] });
-    t.deepEqual(scanner.findNextMatchSync(`X${String.fromCharCode(0xdfff)}X`, 0), { index: 0, captureIndices: [{ start: 0, end: 1, length: 1 }] });
-    t.deepEqual(scanner.findNextMatchSync(`X${String.fromCharCode(0xdfff)}X`, 1), { index: 0, captureIndices: [{ start: 2, end: 3, length: 1 }] });
-    t.deepEqual(scanner.findNextMatchSync(`X${String.fromCharCode(0xdfff)}X`, 2), { index: 0, captureIndices: [{ start: 2, end: 3, length: 1 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync(`X${String.fromCharCode(0xd83c)}X`, 0), { index: 0, captureIndices: [{ start: 0, end: 1, length: 1 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync(`X${String.fromCharCode(0xd83c)}X`, 1), { index: 0, captureIndices: [{ start: 2, end: 3, length: 1 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync(`X${String.fromCharCode(0xd83c)}X`, 2), { index: 0, captureIndices: [{ start: 2, end: 3, length: 1 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync(`X${String.fromCharCode(0xdfff)}X`, 0), { index: 0, captureIndices: [{ start: 0, end: 1, length: 1 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync(`X${String.fromCharCode(0xdfff)}X`, 1), { index: 0, captureIndices: [{ start: 2, end: 3, length: 1 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync(`X${String.fromCharCode(0xdfff)}X`, 2), { index: 0, captureIndices: [{ start: 2, end: 3, length: 1 }] });
     // These are actually valid, just testing the min & max
-    t.deepEqual(scanner.findNextMatchSync(`X${String.fromCharCode(0xd800)}${String.fromCharCode(0xdc00)}X`, 2), { index: 0, captureIndices: [{ start: 3, end: 4, length: 1 }] });
-    t.deepEqual(scanner.findNextMatchSync(`X${String.fromCharCode(0xdbff)}${String.fromCharCode(0xdfff)}X`, 2), { index: 0, captureIndices: [{ start: 3, end: 4, length: 1 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync(`X${String.fromCharCode(0xd800)}${String.fromCharCode(0xdc00)}X`, 2), { index: 0, captureIndices: [{ start: 3, end: 4, length: 1 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync(`X${String.fromCharCode(0xdbff)}${String.fromCharCode(0xdfff)}X`, 2), { index: 0, captureIndices: [{ start: 3, end: 4, length: 1 }] });
     scanner.dispose();
 });
-testLib('out of bounds', (t) => {
+testLib('out of bounds', () => {
     const scanner = new index_1.OnigScanner(['X']);
-    t.deepEqual(scanner.findNextMatchSync(`X💻X`, -1000), { index: 0, captureIndices: [{ start: 0, end: 1, length: 1 }] });
-    t.deepEqual(scanner.findNextMatchSync(`X💻X`, 1000), null);
+    assert.deepStrictEqual(scanner.findNextMatchSync(`X💻X`, -1000), { index: 0, captureIndices: [{ start: 0, end: 1, length: 1 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync(`X💻X`, 1000), null);
     scanner.dispose();
 });
-testLib('regex with \\G', (t) => {
+testLib('regex with \\G', () => {
     const str = new index_1.OnigString('first-and-second');
     const scanner = new index_1.OnigScanner(['\\G-and']);
-    t.deepEqual(scanner.findNextMatchSync(str, 0), null);
-    t.deepEqual(scanner.findNextMatchSync(str, 5), { index: 0, captureIndices: [{ start: 5, end: 9, length: 4 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync(str, 0), null);
+    assert.deepStrictEqual(scanner.findNextMatchSync(str, 5), { index: 0, captureIndices: [{ start: 5, end: 9, length: 4 }] });
     scanner.dispose();
     str.dispose();
 });
-testLib('kkos/oniguruma#192', (t) => {
+testLib('kkos/oniguruma#192', () => {
     const str = new index_1.OnigString("    while (i < len && f(array[i]))");
     const scanner = new index_1.OnigScanner(["(?x)\n  (?<!\\+\\+|--)(?<=[({\\[,?=>:*]|&&|\\|\\||\\?|\\*\\/|^await|[^\\._$[:alnum:]]await|^return|[^\\._$[:alnum:]]return|^default|[^\\._$[:alnum:]]default|^yield|[^\\._$[:alnum:]]yield|^)\\s*\n  (?!<\\s*[_$[:alpha:]][_$[:alnum:]]*((\\s+extends\\s+[^=>])|,)) # look ahead is not type parameter of arrow\n  (?=(<)\\s*(?:([_$[:alpha:]][-_$[:alnum:].]*)(?<!\\.|-)(:))?((?:[a-z][a-z0-9]*|([_$[:alpha:]][-_$[:alnum:].]*))(?<!\\.|-))(?=((<\\s*)|(\\s+))(?!\\?)|\\/?>))"]);
-    t.deepEqual(scanner.findNextMatchSync(str, 0), null);
+    assert.deepStrictEqual(scanner.findNextMatchSync(str, 0), null);
     scanner.dispose();
     str.dispose();
 });
-testLib('FindOption.NotBeginPosition', (t) => {
+testLib('FindOption.NotBeginPosition', () => {
     const str = new index_1.OnigString('first-and-second');
     const scanner = new index_1.OnigScanner(['\\G-and']);
-    t.deepEqual(scanner.findNextMatchSync(str, 5), { index: 0, captureIndices: [{ start: 5, end: 9, length: 4 }] });
-    t.deepEqual(scanner.findNextMatchSync(str, 5, 4 /* NotBeginPosition */), null);
+    assert.deepStrictEqual(scanner.findNextMatchSync(str, 5), { index: 0, captureIndices: [{ start: 5, end: 9, length: 4 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync(str, 5, 4 /* NotBeginPosition */), null);
     scanner.dispose();
     str.dispose();
 });
-testLib('FindOption.NotBeginString', (t) => {
+testLib('FindOption.NotBeginString', () => {
     const str = new index_1.OnigString('first-and-first');
     const scanner = new index_1.OnigScanner(['\\Afirst']);
-    t.deepEqual(scanner.findNextMatchSync(str, 10), null);
-    t.deepEqual(scanner.findNextMatchSync(str, 0), { index: 0, captureIndices: [{ start: 0, end: 5, length: 5 }] });
-    t.deepEqual(scanner.findNextMatchSync(str, 0, 1 /* NotBeginString */), null);
+    assert.deepStrictEqual(scanner.findNextMatchSync(str, 10), null);
+    assert.deepStrictEqual(scanner.findNextMatchSync(str, 0), { index: 0, captureIndices: [{ start: 0, end: 5, length: 5 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync(str, 0, 1 /* NotBeginString */), null);
     scanner.dispose();
     str.dispose();
 });
-testLib('FindOption.NotEndString', (t) => {
+testLib('FindOption.NotEndString', () => {
     const str = new index_1.OnigString('first-and-first');
     const scanner = new index_1.OnigScanner(['first\\z']);
-    t.deepEqual(scanner.findNextMatchSync(str, 10), { index: 0, captureIndices: [{ start: 10, end: 15, length: 5 }] });
-    t.deepEqual(scanner.findNextMatchSync(str, 10, 2 /* NotEndString */), null);
+    assert.deepStrictEqual(scanner.findNextMatchSync(str, 10), { index: 0, captureIndices: [{ start: 10, end: 15, length: 5 }] });
+    assert.deepStrictEqual(scanner.findNextMatchSync(str, 10, 2 /* NotEndString */), null);
     scanner.dispose();
     str.dispose();
 });
