6adcdab003727fd5a0edbbdace0232c15e5e1cd2
Alex Dima <alexdima@microsoft.com>
2024-05-03 15:07:34 +0200

    Fix script

--- build/setup-emsdk.sh
+++ build/setup-emsdk.sh
@@ -3,5 +3,5 @@ mkdir -p /opt/dev \
 && git clone https://github.com/emscripten-core/emsdk.git \
 && cd /opt/dev/emsdk \
 && ./emsdk install 3.1.59 \
-&& ./emsdk activate 3.1.59
+&& ./emsdk activate 3.1.59 \
 && source ./emsdk_env.sh
