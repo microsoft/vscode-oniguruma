c62f64982ae6bf28e0078923748446943573821a
Alex Dima <alexdima@microsoft.com>
2024-05-03 14:05:12 +0200

    Attempt to repair the build

--- build/setup-emsdk.sh
+++ build/setup-emsdk.sh
@@ -2,5 +2,5 @@ mkdir -p /opt/dev \
 && cd /opt/dev \
 && git clone https://github.com/emscripten-core/emsdk.git \
 && cd /opt/dev/emsdk \
-&& ./emsdk install 3.1.45 \
-&& ./emsdk activate 3.1.45
+&& ./emsdk install 3.1.59 \
+&& ./emsdk activate 3.1.59
