22bb17b884c6f7e09d46abfdc7fbc4bd1f0cea34
Alex Dima <alexdima@microsoft.com>
2024-05-03 15:30:56 +0200

    install build essential tools

--- build/setup-emsdk.sh
+++ build/setup-emsdk.sh
@@ -3,5 +3,4 @@ mkdir -p /opt/dev \
 && git clone https://github.com/emscripten-core/emsdk.git \
 && cd /opt/dev/emsdk \
 && ./emsdk install 3.1.59 \
-&& ./emsdk activate 3.1.59 \
-&& source ./emsdk_env.sh
+&& ./emsdk activate 3.1.59
