4eebe21493c0b18659b2a92f42f19458a1cbbd16
Alex Dima <alexdima@microsoft.com>
2024-07-04 12:52:27 +0200

    Make sure builds are consistent and use the same version of the emsdk setup scripts

--- build/setup-emsdk.sh
+++ build/setup-emsdk.sh
@@ -2,5 +2,6 @@ mkdir -p /opt/dev \
 && cd /opt/dev \
 && git clone https://github.com/emscripten-core/emsdk.git \
 && cd /opt/dev/emsdk \
+&& git reset --hard 0fde04880048f743056bed17cb0543a42e040fae \
 && ./emsdk install 3.1.45 \
 && ./emsdk activate 3.1.45
